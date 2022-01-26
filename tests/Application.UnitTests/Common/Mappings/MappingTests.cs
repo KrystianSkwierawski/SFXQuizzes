15625c9ad7cc86780f0e749750a164f02d76459f
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-01-26 15:18:40 +0100

    Add unit tests

---
+++ tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Mappings;
+using Application.Quizzes.Queries;
+using AutoMapper;
+using Domain.Entities;
+using NUnit.Framework;
+using System;
+using System.Runtime.Serialization;
+
+namespace Application.UnitTests.Common.Mappings;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(Quiz), typeof(QuizDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type);
+
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
+
