f9ff7a31fe5da2a11f9865732b2d4140ba05ce58
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-01-26 15:19:35 +0100

    Setup integration tests

---
+++ tests/Application.IntegrationTests/Testing.cs
@@ -0,0 +1,151 @@
+﻿using Infrastructure.Identity;
+using Infrastructure.Persistance;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using Respawn;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI;
+
+namespace Application.IntegrationTests;
+
+[SetUpFixture]
+public class Testing
+{
+    private static IConfigurationRoot _configuration;
+    private static IServiceScopeFactory _scopeFactory;
+    private static Checkpoint _checkpoint;
+
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables();
+
+        _configuration = builder.Build();
+
+        var startup = new Startup(_configuration);
+
+        var services = new ServiceCollection();
+
+        services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+            w.EnvironmentName == "Development" &&
+            w.ApplicationName == "WebUI"));
+
+        services.AddLogging();
+
+        startup.ConfigureServices(services);
+
+        _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
+
+        _checkpoint = new Checkpoint
+        {
+            TablesToIgnore = new[] { "__EFMigrationsHistory" }
+        };
+
+        EnsureDatabase();
+    }
+
+    private static void EnsureDatabase()
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        context.Database.Migrate();
+    }
+
+
+    public static async Task<ApplicationUser> CreateUserAsync()
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+        ApplicationUser user = new()
+        {
+            Email = "test@gmail.com",
+            UserName = Guid.NewGuid().ToString(),
+        };
+
+        var createUserResult = await userManager.CreateAsync(user);
+
+        if (!createUserResult.Succeeded)
+        {
+            var errors = string.Join(Environment.NewLine, createUserResult.Errors.Select(e => e.Description));
+
+            throw new Exception($"Unable to create {user.UserName}.{Environment.NewLine}{errors}");
+        }
+
+        return user;
+    }
+
+
+    public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var mediator = scope.ServiceProvider.GetService<ISender>();
+
+        return await mediator.Send(request);
+    }
+
+    public static async Task ResetState()
+    {
+        await _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    public static async Task<TEntity> FindAsync<TEntity>(params object[] keyValues)
+        where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        return await context.FindAsync<TEntity>(keyValues);
+    }
+
+    public static async Task AddAsync<TEntity>(TEntity entity)
+        where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        context.Add(entity);
+
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task AddRangeAsync<TEntity>(List<TEntity> entities)
+      where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        await context.AddRangeAsync(entities);
+
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task<int> CountAsync<TEntity>() where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        return await context.Set<TEntity>().CountAsync();
+    }
+}
