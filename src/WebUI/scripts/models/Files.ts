b00ed85b8551315458dbb8bff9cf278d923de867
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-01-28 14:13:36 +0100

    Do some refactor

--- src/WebUI/scripts/models/Files.ts
+++ src/WebUI/scripts/models/Files.ts
@@ -1,6 +1,4 @@
-﻿const supportedFormats = ['mp3', 'wav', 'ogg']
-
-export const validateFiles = (files: FileList) => {
+﻿export const validateFiles = (files: FileList) => {
     let validationError: string | undefined;
 
     if (files.length > 30)
@@ -9,6 +7,8 @@ export const validateFiles = (files: FileList) => {
     [...files].forEach(file => {
         if (file.size > 307200)
             validationError = `File size must under 300KB\n\n${file.name}: ${Math.floor(file.size / 1024)}KB \n`;
+
+        const supportedFormats = ['mp3', 'wav', 'ogg'];
 
         const format: string = file.name.split('.')[1];
         if (!supportedFormats.some(supportedFormat => supportedFormat === format.toLowerCase()))
--- src/WebUI/wwwroot/js/models/Files.js
+++ src/WebUI/wwwroot/js/models/Files.js
@@ -1,4 +1,3 @@
-const supportedFormats = ['mp3', 'wav', 'ogg'];
 export const validateFiles = (files) => {
     let validationError;
     if (files.length > 30)
@@ -6,6 +5,7 @@ export const validateFiles = (files) => {
     [...files].forEach(file => {
         if (file.size > 307200)
             validationError = `File size must under 300KB\n\n${file.name}: ${Math.floor(file.size / 1024)}KB \n`;
+        const supportedFormats = ['mp3', 'wav', 'ogg'];
         const format = file.name.split('.')[1];
         if (!supportedFormats.some(supportedFormat => supportedFormat === format.toLowerCase()))
             validationError = `File must be supported audio format - mp3, wav, ogg\n\n${file.name}`;
