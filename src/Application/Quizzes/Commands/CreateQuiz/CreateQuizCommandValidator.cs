2608c366f61a604750def8b7b2564e668313f598
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-01-28 10:25:12 +0100

    Update CreateQuizCommandValidator
    Now it checks the size and the maximum number of files. Should save some disk space.

--- src/Application/Quizzes/Commands/CreateQuiz/CreateQuizCommandValidator.cs
+++ src/Application/Quizzes/Commands/CreateQuiz/CreateQuizCommandValidator.cs
@@ -14,6 +14,15 @@ public class CreateQuizCommandValidator : AbstractValidator<CreateQuizCommand>
         RuleFor(vm => vm.CreateQuizVm.Files)
             .NotNull()
             .NotEmpty();
+
+        // https://www.audiomountain.com/tech/audio-file-size.html
+        // 8Kbps Bitrate per seconds = 1KB. 300 seconds
+        // 320Kbps Bitrate per second = 40KB. 7.5 seconds
+        RuleForEach(vm => vm.CreateQuizVm.Files)
+            .Must(file => file.Length < 300000); // around 300KB for each file
+
+        RuleFor(vm => vm.CreateQuizVm.Files.Count())
+            .LessThanOrEqualTo(30);
     }
 }
 
