02f97bfb4e8cffba759bb0f3b7abce39bc29ef59
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-02-01 12:52:33 +0100

    Add GetUsersQuizzes/QuizDto.cs

---
+++ src/Application/Quizzes/Queries/GetUsersQuizzes/QuizDto.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Mappings;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Quizzes.Queries.GetUsersQuizzes;
+
+public class QuizDto : IMapFrom<Quiz>
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public int NumberOfSFXs { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Quiz, QuizDto>()
+            .ForMember(quizDto => quizDto.NumberOfSFXs, opt => opt.MapFrom(quiz => quiz.SFXs.Count()));
+    }
+}
+
