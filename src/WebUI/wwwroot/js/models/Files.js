709d5725b2843116dc927a98bed65895d5bba9d7
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-02-02 09:03:47 +0100

    Update tsconfig.json

--- src/WebUI/wwwroot/js/models/Files.js
+++ src/WebUI/wwwroot/js/models/Files.js
@@ -1,40 +1,40 @@
-export var validateFiles = function (files) {
-    var numberOfFilesError = validateNumberOfFiles(files);
+export const validateFiles = (files) => {
+    const numberOfFilesError = validateNumberOfFiles(files);
     if (numberOfFilesError)
         return numberOfFilesError;
-    var filesSizeError = validateFilesSize(files);
+    const filesSizeError = validateFilesSize(files);
     if (filesSizeError)
         return filesSizeError;
-    var supportedFormatsError = validateSupportedFormats(files);
+    const supportedFormatsError = validateSupportedFormats(files);
     if (supportedFormatsError)
         return supportedFormatsError;
     return;
 };
-var validateNumberOfFiles = function (files) {
+const validateNumberOfFiles = (files) => {
     if (files.length > 30)
         return 'Number of files must less than 30';
     return;
 };
-var validateFilesSize = function (files) {
-    var tooLargeFiles = Array.from(files).filter(function (file) { return file.size > 300 * 1024; });
+const validateFilesSize = (files) => {
+    const tooLargeFiles = Array.from(files).filter(file => file.size > 300 * 1024);
     if (tooLargeFiles.length > 0) {
-        var validationError_1 = 'File size must under 300KB\n';
-        tooLargeFiles.forEach(function (file) {
-            validationError_1 += "\n".concat(file.name, ": ").concat(Math.floor(file.size / 1024), "KB");
+        let validationError = 'File size must under 300KB\n';
+        tooLargeFiles.forEach(file => {
+            validationError += `\n${file.name}: ${Math.floor(file.size / 1024)}KB`;
         });
-        return validationError_1;
+        return validationError;
     }
     return;
 };
-var validateSupportedFormats = function (files) {
-    var supportedFormats = ['mp3', 'wav', 'ogg'];
-    var notSupportedFiles = Array.from(files).filter(function (file) { return !supportedFormats.includes(file.name.split('.')[1].toLowerCase()); });
+const validateSupportedFormats = (files) => {
+    const supportedFormats = ['mp3', 'wav', 'ogg'];
+    const notSupportedFiles = Array.from(files).filter(file => !supportedFormats.includes(file.name.split('.')[1].toLowerCase()));
     if (notSupportedFiles.length > 0) {
-        var validationError_2 = 'File must be supported audio format - mp3, wav, ogg\n';
-        notSupportedFiles.forEach(function (file) {
-            validationError_2 += "\n".concat(file.name);
+        let validationError = 'File must be supported audio format - mp3, wav, ogg\n';
+        notSupportedFiles.forEach(file => {
+            validationError += `\n${file.name}`;
         });
-        return validationError_2;
+        return validationError;
     }
     return;
 };
--- src/WebUI/wwwroot/js/quizCreator.js
+++ src/WebUI/wwwroot/js/quizCreator.js
@@ -1,41 +1,41 @@
 import { elements } from './views/base.js';
 import * as quizCreatorView from './views/quizCreatorView.js';
 import { validateFiles } from './models/Files.js';
-elements.dropArea.addEventListener('drop', function (e) {
+elements.dropArea.addEventListener('drop', (e) => {
     e.preventDefault();
     quizCreatorView.removeDragEnterBC();
-    var fileList = e.dataTransfer.files;
-    var validationError = validateFiles(fileList);
+    const fileList = e.dataTransfer.files;
+    const validationError = validateFiles(fileList);
     if (validationError)
         return alert(validationError);
     quizCreatorView.setFilesInputValue(fileList);
     quizCreatorView.selectedFilesStatus(fileList);
 });
-elements.filesInput.addEventListener('change', function (e) {
-    var fileList = quizCreatorView.getFilesInputValue();
-    var validationError = validateFiles(fileList);
+elements.filesInput.addEventListener('change', (e) => {
+    const fileList = quizCreatorView.getFilesInputValue();
+    const validationError = validateFiles(fileList);
     if (validationError) {
         quizCreatorView.resetFilesInput();
         return alert(validationError);
     }
     quizCreatorView.selectedFilesStatus(fileList);
 });
-elements.dropArea.addEventListener('dragenter', function (e) {
+elements.dropArea.addEventListener('dragenter', (e) => {
     e.preventDefault();
     quizCreatorView.addDragEnterBC();
 });
-elements.dropArea.addEventListener('dragleave', function (e) {
+elements.dropArea.addEventListener('dragleave', (e) => {
     e.preventDefault();
     quizCreatorView.removeDragEnterBC();
 });
-elements.dropArea.addEventListener('dragover', function (e) {
+elements.dropArea.addEventListener('dragover', (e) => {
     e.preventDefault();
 });
-elements.dropArea.addEventListener('dragstart', function (e) {
+elements.dropArea.addEventListener('dragstart', (e) => {
     e.preventDefault();
     e.dataTransfer.setData("file", "data");
 });
-elements.browseFilesButton.addEventListener('click', function () {
+elements.browseFilesButton.addEventListener('click', () => {
     elements.filesInput.click();
 });
 //# sourceMappingURL=quizCreator.js.map
