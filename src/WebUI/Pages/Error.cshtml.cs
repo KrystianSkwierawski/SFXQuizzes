2ba26fb2138cbc5157b55c30899fe01359c5e372
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2022-02-21 12:58:15 +0100

    Change project name

--- src/WebUI/Pages/Error.cshtml.cs
+++ src/WebUI/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace SFXQuizzes.Pages
+namespace SQuizzes.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
